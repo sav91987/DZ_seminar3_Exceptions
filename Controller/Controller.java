915b1d0240a64446c87e6286f2ba2770b76b29f9
Sotnikov_Andrey <sav91987@ya.ru>
2023-07-31 16:11:21 +0300

    some changes

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -5,38 +5,38 @@ import Model.PowerCalculator;
 import View.Printer;
 
 public class Controller {
-    
+
     public void run(int firstNum, int secondNum) {
 
         Printer<? super Object> print = new Printer<>();
 
         PowerCalculator operation = new PowerCalculator();
         try {
-            print.print(operation.sum(firstNum, secondNum));
+            print.print("Результат сложения: " + operation.sum(firstNum, secondNum));
         } catch (ArithmeticException e) {
             System.out.println(e.getMessage() + "Переполнение типа int!");
         }
 
         try {
-            print.print(operation.sub(firstNum, secondNum));
+            print.print("Результат вычитания: " + operation.sub(firstNum, secondNum));
         } catch (ArithmeticException e) {
             System.out.println(e.getMessage() + "Уменьшаемое меньше вычитаемого!");
         }
 
         try {
-            print.print(operation.multi(firstNum, secondNum));
+            print.print("Результат умножения: " + operation.multi(firstNum, secondNum));
         } catch (ArithmeticException e) {
             System.out.println(e.getMessage() + "Переполнение типа int!");
         }
 
         try {
-            print.print(operation.div(firstNum, secondNum));
+            print.print("Результат деления: " + operation.div(firstNum, secondNum));
         } catch (ArithmeticException e) {
             System.out.println(e.getMessage() + "Деление на 0!");
         }
 
         try {
-            print.print(operation.calculatePower(firstNum, secondNum));
+            print.print("Результат возведения в степень: " + operation.calculatePower(firstNum, secondNum));
         } catch (InvalidInputException e) {
             System.out.println(e.getMessage() + "Некорретное основание или степень");
         }
